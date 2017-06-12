440ed1639d71502269e07bd15890c79471135827
dilum1995 <dilumdesilva.edu@gmail.com>
2017-06-12 13:15:56 +0530

    Form edited

--- UniversityRegistrationSystem/UniversityRegistrationSystem/statusSelectionForm.Designer.cs
+++ UniversityRegistrationSystem/UniversityRegistrationSystem/statusSelectionForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UniversityRegistrationSystem
 {
-    partial class MainForm
+    partial class statusSelectionForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,75 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(statusSelectionForm));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.picBoxLogoMainForm = new System.Windows.Forms.PictureBox();
-            this.btnSignup = new System.Windows.Forms.Button();
-            this.btnRegister = new System.Windows.Forms.Button();
-            this.lblCopyright1 = new System.Windows.Forms.Label();
             this.lblCopyright2 = new System.Windows.Forms.Label();
+            this.lblCopyright1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxLogoMainForm)).BeginInit();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(55, 171);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(139, 21);
+            this.comboBox1.TabIndex = 0;
             // 
             // picBoxLogoMainForm
             // 
             this.picBoxLogoMainForm.Image = ((System.Drawing.Image)(resources.GetObject("picBoxLogoMainForm.Image")));
-            this.picBoxLogoMainForm.Location = new System.Drawing.Point(23, 34);
+            this.picBoxLogoMainForm.Location = new System.Drawing.Point(55, 28);
             this.picBoxLogoMainForm.Name = "picBoxLogoMainForm";
             this.picBoxLogoMainForm.Size = new System.Drawing.Size(209, 67);
             this.picBoxLogoMainForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picBoxLogoMainForm.TabIndex = 0;
+            this.picBoxLogoMainForm.TabIndex = 1;
             this.picBoxLogoMainForm.TabStop = false;
-            // 
-            // btnSignup
-            // 
-            this.btnSignup.Location = new System.Drawing.Point(76, 213);
-            this.btnSignup.Name = "btnSignup";
-            this.btnSignup.Size = new System.Drawing.Size(97, 35);
-            this.btnSignup.TabIndex = 3;
-            this.btnSignup.Text = "Sign up";
-            this.btnSignup.UseVisualStyleBackColor = true;
-            // 
-            // btnRegister
-            // 
-            this.btnRegister.Location = new System.Drawing.Point(76, 162);
-            this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(97, 35);
-            this.btnRegister.TabIndex = 4;
-            this.btnRegister.Text = "Register";
-            this.btnRegister.UseVisualStyleBackColor = true;
-            // 
-            // lblCopyright1
-            // 
-            this.lblCopyright1.AutoSize = true;
-            this.lblCopyright1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCopyright1.Location = new System.Drawing.Point(88, 304);
-            this.lblCopyright1.Name = "lblCopyright1";
-            this.lblCopyright1.Size = new System.Drawing.Size(45, 12);
-            this.lblCopyright1.TabIndex = 5;
-            this.lblCopyright1.Text = "Copyright";
             // 
             // lblCopyright2
             // 
             this.lblCopyright2.AutoSize = true;
             this.lblCopyright2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCopyright2.Location = new System.Drawing.Point(131, 308);
+            this.lblCopyright2.Location = new System.Drawing.Point(162, 245);
             this.lblCopyright2.Name = "lblCopyright2";
             this.lblCopyright2.Size = new System.Drawing.Size(27, 7);
-            this.lblCopyright2.TabIndex = 6;
+            this.lblCopyright2.TabIndex = 8;
             this.lblCopyright2.Text = "C 2017";
             // 
-            // MainForm
+            // lblCopyright1
+            // 
+            this.lblCopyright1.AutoSize = true;
+            this.lblCopyright1.Location = new System.Drawing.Point(115, 240);
+            this.lblCopyright1.Name = "lblCopyright1";
+            this.lblCopyright1.Size = new System.Drawing.Size(51, 13);
+            this.lblCopyright1.TabIndex = 7;
+            this.lblCopyright1.Text = "Copyright";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(52, 135);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(124, 13);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "Please select your status";
+            // 
+            // statusSelectionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(256, 326);
+            this.ClientSize = new System.Drawing.Size(312, 262);
+            this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.lblCopyright2);
             this.Controls.Add(this.lblCopyright1);
-            this.Controls.Add(this.btnRegister);
-            this.Controls.Add(this.btnSignup);
             this.Controls.Add(this.picBoxLogoMainForm);
-            this.Name = "MainForm";
-            this.Text = "UoW";
+            this.Controls.Add(this.comboBox1);
+            this.Name = "statusSelectionForm";
+            this.Text = "statusSelectionForm";
             ((System.ComponentModel.ISupportInitialize)(this.picBoxLogoMainForm)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -105,11 +103,10 @@
 
         #endregion
 
+        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.PictureBox picBoxLogoMainForm;
-        private System.Windows.Forms.Button btnSignup;
-        private System.Windows.Forms.Button btnRegister;
-        private System.Windows.Forms.Label lblCopyright1;
         private System.Windows.Forms.Label lblCopyright2;
+        private System.Windows.Forms.Label lblCopyright1;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
-
