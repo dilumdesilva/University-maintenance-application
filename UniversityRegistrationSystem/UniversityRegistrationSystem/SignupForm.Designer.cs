341469ed9655c2d6cf73b2f89a197c41ccce90fa
dilum1995 <dilumdesilva.edu@gmail.com>
2017-06-14 10:57:37 +0530

    signup form changes done

---
+++ UniversityRegistrationSystem/UniversityRegistrationSystem/SignupForm.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace UniversityRegistrationSystem
+{
+    partial class SignupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignupForm));
+            this.txtPassword1 = new System.Windows.Forms.TextBox();
+            this.lblStuPassword = new System.Windows.Forms.Label();
+            this.lblCopyright2 = new System.Windows.Forms.Label();
+            this.lblCopyright1 = new System.Windows.Forms.Label();
+            this.picBoxLogoMainForm = new System.Windows.Forms.PictureBox();
+            this.comboDepartment = new System.Windows.Forms.ComboBox();
+            this.btnStudentLogin = new System.Windows.Forms.Button();
+            this.txtUniversityID = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblStuDepartment = new System.Windows.Forms.Label();
+            this.lblStudentID = new System.Windows.Forms.Label();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboStatus = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword2 = new System.Windows.Forms.TextBox();
+            this.lblConfirmPass = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogoMainForm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPassword1
+            // 
+            this.txtPassword1.Location = new System.Drawing.Point(167, 281);
+            this.txtPassword1.Name = "txtPassword1";
+            this.txtPassword1.Size = new System.Drawing.Size(144, 20);
+            this.txtPassword1.TabIndex = 48;
+            // 
+            // lblStuPassword
+            // 
+            this.lblStuPassword.AutoSize = true;
+            this.lblStuPassword.BackColor = System.Drawing.Color.Transparent;
+            this.lblStuPassword.Location = new System.Drawing.Point(46, 284);
+            this.lblStuPassword.Name = "lblStuPassword";
+            this.lblStuPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblStuPassword.TabIndex = 47;
+            this.lblStuPassword.Text = "Password";
+            // 
+            // lblCopyright2
+            // 
+            this.lblCopyright2.AutoSize = true;
+            this.lblCopyright2.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright2.Location = new System.Drawing.Point(178, 471);
+            this.lblCopyright2.Name = "lblCopyright2";
+            this.lblCopyright2.Size = new System.Drawing.Size(27, 7);
+            this.lblCopyright2.TabIndex = 46;
+            this.lblCopyright2.Text = "C 2017";
+            // 
+            // lblCopyright1
+            // 
+            this.lblCopyright1.AutoSize = true;
+            this.lblCopyright1.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright1.Location = new System.Drawing.Point(135, 467);
+            this.lblCopyright1.Name = "lblCopyright1";
+            this.lblCopyright1.Size = new System.Drawing.Size(45, 12);
+            this.lblCopyright1.TabIndex = 45;
+            this.lblCopyright1.Text = "Copyright";
+            // 
+            // picBoxLogoMainForm
+            // 
+            this.picBoxLogoMainForm.BackColor = System.Drawing.Color.Transparent;
+            this.picBoxLogoMainForm.Image = ((System.Drawing.Image)(resources.GetObject("picBoxLogoMainForm.Image")));
+            this.picBoxLogoMainForm.Location = new System.Drawing.Point(80, 30);
+            this.picBoxLogoMainForm.Name = "picBoxLogoMainForm";
+            this.picBoxLogoMainForm.Size = new System.Drawing.Size(189, 53);
+            this.picBoxLogoMainForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxLogoMainForm.TabIndex = 44;
+            this.picBoxLogoMainForm.TabStop = false;
+            // 
+            // comboDepartment
+            // 
+            this.comboDepartment.FormattingEnabled = true;
+            this.comboDepartment.Location = new System.Drawing.Point(167, 351);
+            this.comboDepartment.Name = "comboDepartment";
+            this.comboDepartment.Size = new System.Drawing.Size(144, 21);
+            this.comboDepartment.TabIndex = 43;
+            // 
+            // btnStudentLogin
+            // 
+            this.btnStudentLogin.Location = new System.Drawing.Point(138, 402);
+            this.btnStudentLogin.Name = "btnStudentLogin";
+            this.btnStudentLogin.Size = new System.Drawing.Size(80, 27);
+            this.btnStudentLogin.TabIndex = 42;
+            this.btnStudentLogin.Text = "SignUp";
+            this.btnStudentLogin.UseVisualStyleBackColor = true;
+            // 
+            // txtUniversityID
+            // 
+            this.txtUniversityID.Location = new System.Drawing.Point(167, 246);
+            this.txtUniversityID.Name = "txtUniversityID";
+            this.txtUniversityID.Size = new System.Drawing.Size(144, 20);
+            this.txtUniversityID.TabIndex = 41;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(167, 143);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(144, 20);
+            this.txtFirstName.TabIndex = 40;
+            // 
+            // lblStuDepartment
+            // 
+            this.lblStuDepartment.AutoSize = true;
+            this.lblStuDepartment.BackColor = System.Drawing.Color.Transparent;
+            this.lblStuDepartment.Location = new System.Drawing.Point(47, 354);
+            this.lblStuDepartment.Name = "lblStuDepartment";
+            this.lblStuDepartment.Size = new System.Drawing.Size(62, 13);
+            this.lblStuDepartment.TabIndex = 39;
+            this.lblStuDepartment.Text = "Department";
+            // 
+            // lblStudentID
+            // 
+            this.lblStudentID.AutoSize = true;
+            this.lblStudentID.BackColor = System.Drawing.Color.Transparent;
+            this.lblStudentID.Location = new System.Drawing.Point(46, 249);
+            this.lblStudentID.Name = "lblStudentID";
+            this.lblStudentID.Size = new System.Drawing.Size(67, 13);
+            this.lblStudentID.TabIndex = 38;
+            this.lblStudentID.Text = "University ID";
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.BackColor = System.Drawing.Color.Transparent;
+            this.lblStudentName.Location = new System.Drawing.Point(47, 146);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(57, 13);
+            this.lblStudentName.TabIndex = 37;
+            this.lblStudentName.Text = "First Name";
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.BackColor = System.Drawing.Color.Transparent;
+            this.lblInstructions.Location = new System.Drawing.Point(46, 112);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(122, 13);
+            this.lblInstructions.TabIndex = 36;
+            this.lblInstructions.Text = "Please enter your details";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(167, 176);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(144, 20);
+            this.txtLastName.TabIndex = 50;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(47, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "Last Name";
+            // 
+            // comboStatus
+            // 
+            this.comboStatus.FormattingEnabled = true;
+            this.comboStatus.Location = new System.Drawing.Point(167, 212);
+            this.comboStatus.Name = "comboStatus";
+            this.comboStatus.Size = new System.Drawing.Size(144, 21);
+            this.comboStatus.TabIndex = 52;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(47, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 51;
+            this.label2.Text = "Status";
+            // 
+            // txtPassword2
+            // 
+            this.txtPassword2.Location = new System.Drawing.Point(167, 316);
+            this.txtPassword2.Name = "txtPassword2";
+            this.txtPassword2.Size = new System.Drawing.Size(144, 20);
+            this.txtPassword2.TabIndex = 54;
+            // 
+            // lblConfirmPass
+            // 
+            this.lblConfirmPass.AutoSize = true;
+            this.lblConfirmPass.BackColor = System.Drawing.Color.Transparent;
+            this.lblConfirmPass.Location = new System.Drawing.Point(46, 319);
+            this.lblConfirmPass.Name = "lblConfirmPass";
+            this.lblConfirmPass.Size = new System.Drawing.Size(90, 13);
+            this.lblConfirmPass.TabIndex = 53;
+            this.lblConfirmPass.Text = "Confirm password";
+            // 
+            // SignupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(354, 489);
+            this.Controls.Add(this.txtPassword2);
+            this.Controls.Add(this.lblConfirmPass);
+            this.Controls.Add(this.comboStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword1);
+            this.Controls.Add(this.lblStuPassword);
+            this.Controls.Add(this.lblCopyright2);
+            this.Controls.Add(this.lblCopyright1);
+            this.Controls.Add(this.picBoxLogoMainForm);
+            this.Controls.Add(this.comboDepartment);
+            this.Controls.Add(this.btnStudentLogin);
+            this.Controls.Add(this.txtUniversityID);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblStuDepartment);
+            this.Controls.Add(this.lblStudentID);
+            this.Controls.Add(this.lblStudentName);
+            this.Controls.Add(this.lblInstructions);
+            this.Name = "SignupForm";
+            this.Text = "SignupForm";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogoMainForm)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPassword1;
+        private System.Windows.Forms.Label lblStuPassword;
+        private System.Windows.Forms.Label lblCopyright2;
+        private System.Windows.Forms.Label lblCopyright1;
+        private System.Windows.Forms.PictureBox picBoxLogoMainForm;
+        private System.Windows.Forms.ComboBox comboDepartment;
+        private System.Windows.Forms.Button btnStudentLogin;
+        private System.Windows.Forms.TextBox txtUniversityID;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblStuDepartment;
+        private System.Windows.Forms.Label lblStudentID;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword2;
+        private System.Windows.Forms.Label lblConfirmPass;
+    }
+}
