14144e8cda2272c6f8c86f29b5ce6b4c722a721b
dilum1995 <dilumdesilva.edu@gmail.com>
2017-06-14 08:53:13 +0530

    student's main form changes

--- UniversityRegistrationSystem/UniversityRegistrationSystem/studentMainForm.Designer.cs
+++ UniversityRegistrationSystem/UniversityRegistrationSystem/studentMainForm.Designer.cs
@@ -38,6 +38,8 @@
             this.lblStudentNameView = new System.Windows.Forms.Label();
             this.lblStudentIDView = new System.Windows.Forms.Label();
             this.lblDepartmentView = new System.Windows.Forms.Label();
+            this.lblCopyright2 = new System.Windows.Forms.Label();
+            this.lblCopyright1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxLogoMainForm)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,7 +47,7 @@
             // 
             this.picBoxLogoMainForm.BackColor = System.Drawing.Color.Transparent;
             this.picBoxLogoMainForm.Image = ((System.Drawing.Image)(resources.GetObject("picBoxLogoMainForm.Image")));
-            this.picBoxLogoMainForm.Location = new System.Drawing.Point(62, 12);
+            this.picBoxLogoMainForm.Location = new System.Drawing.Point(62, 14);
             this.picBoxLogoMainForm.Name = "picBoxLogoMainForm";
             this.picBoxLogoMainForm.Size = new System.Drawing.Size(189, 53);
             this.picBoxLogoMainForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -56,7 +58,7 @@
             // 
             this.lblStudentName.AutoSize = true;
             this.lblStudentName.BackColor = System.Drawing.Color.Transparent;
-            this.lblStudentName.Location = new System.Drawing.Point(76, 95);
+            this.lblStudentName.Location = new System.Drawing.Point(69, 108);
             this.lblStudentName.Name = "lblStudentName";
             this.lblStudentName.Size = new System.Drawing.Size(81, 13);
             this.lblStudentName.TabIndex = 11;
@@ -66,7 +68,7 @@
             // 
             this.lblStudentID.AutoSize = true;
             this.lblStudentID.BackColor = System.Drawing.Color.Transparent;
-            this.lblStudentID.Location = new System.Drawing.Point(76, 127);
+            this.lblStudentID.Location = new System.Drawing.Point(69, 140);
             this.lblStudentID.Name = "lblStudentID";
             this.lblStudentID.Size = new System.Drawing.Size(64, 13);
             this.lblStudentID.TabIndex = 12;
@@ -76,7 +78,7 @@
             // 
             this.lblDepartment.AutoSize = true;
             this.lblDepartment.BackColor = System.Drawing.Color.Transparent;
-            this.lblDepartment.Location = new System.Drawing.Point(76, 157);
+            this.lblDepartment.Location = new System.Drawing.Point(69, 170);
             this.lblDepartment.Name = "lblDepartment";
             this.lblDepartment.Size = new System.Drawing.Size(68, 13);
             this.lblDepartment.TabIndex = 13;
@@ -84,7 +86,7 @@
             // 
             // btnCheckMark
             // 
-            this.btnCheckMark.Location = new System.Drawing.Point(62, 205);
+            this.btnCheckMark.Location = new System.Drawing.Point(106, 214);
             this.btnCheckMark.Name = "btnCheckMark";
             this.btnCheckMark.Size = new System.Drawing.Size(92, 31);
             this.btnCheckMark.TabIndex = 14;
@@ -93,7 +95,7 @@
             // 
             // btnLogout
             // 
-            this.btnLogout.Location = new System.Drawing.Point(178, 205);
+            this.btnLogout.Location = new System.Drawing.Point(106, 254);
             this.btnLogout.Name = "btnLogout";
             this.btnLogout.Size = new System.Drawing.Size(92, 31);
             this.btnLogout.TabIndex = 15;
@@ -104,7 +106,7 @@
             // 
             this.lblStudentNameView.AutoSize = true;
             this.lblStudentNameView.BackColor = System.Drawing.Color.Transparent;
-            this.lblStudentNameView.Location = new System.Drawing.Point(165, 95);
+            this.lblStudentNameView.Location = new System.Drawing.Point(158, 108);
             this.lblStudentNameView.Name = "lblStudentNameView";
             this.lblStudentNameView.Size = new System.Drawing.Size(75, 13);
             this.lblStudentNameView.TabIndex = 16;
@@ -114,7 +116,7 @@
             // 
             this.lblStudentIDView.AutoSize = true;
             this.lblStudentIDView.BackColor = System.Drawing.Color.Transparent;
-            this.lblStudentIDView.Location = new System.Drawing.Point(165, 127);
+            this.lblStudentIDView.Location = new System.Drawing.Point(158, 140);
             this.lblStudentIDView.Name = "lblStudentIDView";
             this.lblStudentIDView.Size = new System.Drawing.Size(56, 13);
             this.lblStudentIDView.TabIndex = 17;
@@ -124,18 +126,42 @@
             // 
             this.lblDepartmentView.AutoSize = true;
             this.lblDepartmentView.BackColor = System.Drawing.Color.Transparent;
-            this.lblDepartmentView.Location = new System.Drawing.Point(165, 157);
+            this.lblDepartmentView.Location = new System.Drawing.Point(158, 170);
             this.lblDepartmentView.Name = "lblDepartmentView";
             this.lblDepartmentView.Size = new System.Drawing.Size(86, 13);
             this.lblDepartmentView.TabIndex = 18;
             this.lblDepartmentView.Text = "Department here";
+            // 
+            // lblCopyright2
+            // 
+            this.lblCopyright2.AutoSize = true;
+            this.lblCopyright2.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright2.Location = new System.Drawing.Point(162, 327);
+            this.lblCopyright2.Name = "lblCopyright2";
+            this.lblCopyright2.Size = new System.Drawing.Size(27, 7);
+            this.lblCopyright2.TabIndex = 24;
+            this.lblCopyright2.Text = "C 2017";
+            // 
+            // lblCopyright1
+            // 
+            this.lblCopyright1.AutoSize = true;
+            this.lblCopyright1.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright1.Location = new System.Drawing.Point(119, 323);
+            this.lblCopyright1.Name = "lblCopyright1";
+            this.lblCopyright1.Size = new System.Drawing.Size(45, 12);
+            this.lblCopyright1.TabIndex = 23;
+            this.lblCopyright1.Text = "Copyright";
             // 
             // studentMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(317, 261);
+            this.ClientSize = new System.Drawing.Size(317, 343);
+            this.Controls.Add(this.lblCopyright2);
+            this.Controls.Add(this.lblCopyright1);
             this.Controls.Add(this.lblDepartmentView);
             this.Controls.Add(this.lblStudentIDView);
             this.Controls.Add(this.lblStudentNameView);
@@ -165,5 +191,7 @@
         private System.Windows.Forms.Label lblStudentNameView;
         private System.Windows.Forms.Label lblStudentIDView;
         private System.Windows.Forms.Label lblDepartmentView;
+        private System.Windows.Forms.Label lblCopyright2;
+        private System.Windows.Forms.Label lblCopyright1;
     }
 }
