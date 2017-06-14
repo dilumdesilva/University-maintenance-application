86c4ba539a5aa1efab519aac73a0f2930fafce4c
dilum1995 <dilumdesilva.edu@gmail.com>
2017-06-14 08:46:10 +0530

    lectrer marks entering form edited

--- UniversityRegistrationSystem/UniversityRegistrationSystem/lecturerAddMarksForm.Designer.cs
+++ UniversityRegistrationSystem/UniversityRegistrationSystem/lecturerAddMarksForm.Designer.cs
@@ -29,21 +29,153 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lecturerAddMarksForm));
+            this.picBoxLogoMainForm = new System.Windows.Forms.PictureBox();
+            this.btnLecBacktoLogin = new System.Windows.Forms.Button();
+            this.btnEmailGenerator = new System.Windows.Forms.Button();
+            this.dgvStudentsMarks = new System.Windows.Forms.DataGridView();
+            this.lblCopyright2 = new System.Windows.Forms.Label();
+            this.lblCopyright1 = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.comboDepartment = new System.Windows.Forms.ComboBox();
+            this.lblNameReplace = new System.Windows.Forms.Label();
+            this.btnUpdateLecMarks = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogoMainForm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentsMarks)).BeginInit();
             this.SuspendLayout();
+            // 
+            // picBoxLogoMainForm
+            // 
+            this.picBoxLogoMainForm.BackColor = System.Drawing.Color.Transparent;
+            this.picBoxLogoMainForm.Image = ((System.Drawing.Image)(resources.GetObject("picBoxLogoMainForm.Image")));
+            this.picBoxLogoMainForm.Location = new System.Drawing.Point(124, 21);
+            this.picBoxLogoMainForm.Name = "picBoxLogoMainForm";
+            this.picBoxLogoMainForm.Size = new System.Drawing.Size(189, 53);
+            this.picBoxLogoMainForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxLogoMainForm.TabIndex = 17;
+            this.picBoxLogoMainForm.TabStop = false;
+            // 
+            // btnLecBacktoLogin
+            // 
+            this.btnLecBacktoLogin.Location = new System.Drawing.Point(327, 318);
+            this.btnLecBacktoLogin.Name = "btnLecBacktoLogin";
+            this.btnLecBacktoLogin.Size = new System.Drawing.Size(97, 29);
+            this.btnLecBacktoLogin.TabIndex = 15;
+            this.btnLecBacktoLogin.Text = "Home";
+            this.btnLecBacktoLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnEmailGenerator
+            // 
+            this.btnEmailGenerator.Location = new System.Drawing.Point(129, 318);
+            this.btnEmailGenerator.Name = "btnEmailGenerator";
+            this.btnEmailGenerator.Size = new System.Drawing.Size(84, 29);
+            this.btnEmailGenerator.TabIndex = 14;
+            this.btnEmailGenerator.Text = "Email a report";
+            this.btnEmailGenerator.UseVisualStyleBackColor = true;
+            // 
+            // dgvStudentsMarks
+            // 
+            this.dgvStudentsMarks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudentsMarks.Location = new System.Drawing.Point(39, 141);
+            this.dgvStudentsMarks.Name = "dgvStudentsMarks";
+            this.dgvStudentsMarks.Size = new System.Drawing.Size(385, 156);
+            this.dgvStudentsMarks.TabIndex = 13;
+            // 
+            // lblCopyright2
+            // 
+            this.lblCopyright2.AutoSize = true;
+            this.lblCopyright2.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright2.Location = new System.Drawing.Point(248, 377);
+            this.lblCopyright2.Name = "lblCopyright2";
+            this.lblCopyright2.Size = new System.Drawing.Size(27, 7);
+            this.lblCopyright2.TabIndex = 22;
+            this.lblCopyright2.Text = "C 2017";
+            // 
+            // lblCopyright1
+            // 
+            this.lblCopyright1.AutoSize = true;
+            this.lblCopyright1.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright1.Location = new System.Drawing.Point(205, 373);
+            this.lblCopyright1.Name = "lblCopyright1";
+            this.lblCopyright1.Size = new System.Drawing.Size(45, 12);
+            this.lblCopyright1.TabIndex = 21;
+            this.lblCopyright1.Text = "Copyright";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.BackColor = System.Drawing.Color.Transparent;
+            this.lblMessage.Location = new System.Drawing.Point(66, 108);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(151, 13);
+            this.lblMessage.TabIndex = 25;
+            this.lblMessage.Text = ",please select your department";
+            // 
+            // comboDepartment
+            // 
+            this.comboDepartment.FormattingEnabled = true;
+            this.comboDepartment.Location = new System.Drawing.Point(266, 105);
+            this.comboDepartment.Name = "comboDepartment";
+            this.comboDepartment.Size = new System.Drawing.Size(158, 21);
+            this.comboDepartment.TabIndex = 24;
+            // 
+            // lblNameReplace
+            // 
+            this.lblNameReplace.AutoSize = true;
+            this.lblNameReplace.BackColor = System.Drawing.Color.Transparent;
+            this.lblNameReplace.Location = new System.Drawing.Point(36, 108);
+            this.lblNameReplace.Name = "lblNameReplace";
+            this.lblNameReplace.Size = new System.Drawing.Size(35, 13);
+            this.lblNameReplace.TabIndex = 23;
+            this.lblNameReplace.Text = "Name";
+            // 
+            // btnUpdateLecMarks
+            // 
+            this.btnUpdateLecMarks.Location = new System.Drawing.Point(39, 318);
+            this.btnUpdateLecMarks.Name = "btnUpdateLecMarks";
+            this.btnUpdateLecMarks.Size = new System.Drawing.Size(84, 29);
+            this.btnUpdateLecMarks.TabIndex = 26;
+            this.btnUpdateLecMarks.Text = "Update";
+            this.btnUpdateLecMarks.UseVisualStyleBackColor = true;
             // 
             // lecturerAddMarksForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(467, 393);
+            this.Controls.Add(this.btnUpdateLecMarks);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.comboDepartment);
+            this.Controls.Add(this.lblNameReplace);
+            this.Controls.Add(this.lblCopyright2);
+            this.Controls.Add(this.lblCopyright1);
+            this.Controls.Add(this.picBoxLogoMainForm);
+            this.Controls.Add(this.btnLecBacktoLogin);
+            this.Controls.Add(this.btnEmailGenerator);
+            this.Controls.Add(this.dgvStudentsMarks);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "lecturerAddMarksForm";
             this.Text = "lecturerAddMarksForm";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogoMainForm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentsMarks)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picBoxLogoMainForm;
+        private System.Windows.Forms.Button btnLecBacktoLogin;
+        private System.Windows.Forms.Button btnEmailGenerator;
+        private System.Windows.Forms.DataGridView dgvStudentsMarks;
+        private System.Windows.Forms.Label lblCopyright2;
+        private System.Windows.Forms.Label lblCopyright1;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.ComboBox comboDepartment;
+        private System.Windows.Forms.Label lblNameReplace;
+        private System.Windows.Forms.Button btnUpdateLecMarks;
     }
 }
