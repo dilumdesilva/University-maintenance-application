1d374d825543f8daaae3f2273f53cc7401f4098c
dilum1995 <dilumdesilva.edu@gmail.com>
2017-06-14 08:35:35 +0530

    student form edited

--- UniversityRegistrationSystem/UniversityRegistrationSystem/studentLoginForm.Designer.cs
+++ UniversityRegistrationSystem/UniversityRegistrationSystem/studentLoginForm.Designer.cs
@@ -29,112 +29,159 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(studentLoginForm));
-            this.lblInstructions = new System.Windows.Forms.Label();
-            this.lblStudentName = new System.Windows.Forms.Label();
-            this.lblStudentID = new System.Windows.Forms.Label();
-            this.lblDepartment = new System.Windows.Forms.Label();
-            this.txtStudentName = new System.Windows.Forms.TextBox();
-            this.txtStudentID = new System.Windows.Forms.TextBox();
-            this.btnLoginToCheckMarks = new System.Windows.Forms.Button();
-            this.comboDepartment = new System.Windows.Forms.ComboBox();
+            this.txtStuPassword = new System.Windows.Forms.TextBox();
+            this.lblStuPassword = new System.Windows.Forms.Label();
+            this.lblCopyright2 = new System.Windows.Forms.Label();
+            this.lblCopyright1 = new System.Windows.Forms.Label();
             this.picBoxLogoMainForm = new System.Windows.Forms.PictureBox();
+            this.comboStuDepartment = new System.Windows.Forms.ComboBox();
+            this.btnLecturerLogin = new System.Windows.Forms.Button();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.lblStuDepartment = new System.Windows.Forms.Label();
+            this.lblStudentID = new System.Windows.Forms.Label();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.lblInstructions = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxLogoMainForm)).BeginInit();
             this.SuspendLayout();
             // 
-            // lblInstructions
+            // txtStuPassword
             // 
-            this.lblInstructions.AutoSize = true;
-            this.lblInstructions.BackColor = System.Drawing.Color.Transparent;
-            this.lblInstructions.Location = new System.Drawing.Point(37, 94);
-            this.lblInstructions.Name = "lblInstructions";
-            this.lblInstructions.Size = new System.Drawing.Size(122, 13);
-            this.lblInstructions.TabIndex = 0;
-            this.lblInstructions.Text = "Please enter your details";
+            this.txtStuPassword.Location = new System.Drawing.Point(120, 201);
+            this.txtStuPassword.Name = "txtStuPassword";
+            this.txtStuPassword.Size = new System.Drawing.Size(144, 20);
+            this.txtStuPassword.TabIndex = 35;
             // 
-            // lblStudentName
+            // lblStuPassword
             // 
-            this.lblStudentName.AutoSize = true;
-            this.lblStudentName.BackColor = System.Drawing.Color.Transparent;
-            this.lblStudentName.Location = new System.Drawing.Point(38, 128);
-            this.lblStudentName.Name = "lblStudentName";
-            this.lblStudentName.Size = new System.Drawing.Size(35, 13);
-            this.lblStudentName.TabIndex = 1;
-            this.lblStudentName.Text = "Name";
+            this.lblStuPassword.AutoSize = true;
+            this.lblStuPassword.BackColor = System.Drawing.Color.Transparent;
+            this.lblStuPassword.Location = new System.Drawing.Point(28, 204);
+            this.lblStuPassword.Name = "lblStuPassword";
+            this.lblStuPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblStuPassword.TabIndex = 34;
+            this.lblStuPassword.Text = "Password";
             // 
-            // lblStudentID
+            // lblCopyright2
             // 
-            this.lblStudentID.AutoSize = true;
-            this.lblStudentID.BackColor = System.Drawing.Color.Transparent;
-            this.lblStudentID.Location = new System.Drawing.Point(37, 160);
-            this.lblStudentID.Name = "lblStudentID";
-            this.lblStudentID.Size = new System.Drawing.Size(58, 13);
-            this.lblStudentID.TabIndex = 2;
-            this.lblStudentID.Text = "Student ID";
+            this.lblCopyright2.AutoSize = true;
+            this.lblCopyright2.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright2.Location = new System.Drawing.Point(165, 354);
+            this.lblCopyright2.Name = "lblCopyright2";
+            this.lblCopyright2.Size = new System.Drawing.Size(27, 7);
+            this.lblCopyright2.TabIndex = 33;
+            this.lblCopyright2.Text = "C 2017";
             // 
-            // lblDepartment
+            // lblCopyright1
             // 
-            this.lblDepartment.AutoSize = true;
-            this.lblDepartment.BackColor = System.Drawing.Color.Transparent;
-            this.lblDepartment.Location = new System.Drawing.Point(38, 191);
-            this.lblDepartment.Name = "lblDepartment";
-            this.lblDepartment.Size = new System.Drawing.Size(62, 13);
-            this.lblDepartment.TabIndex = 3;
-            this.lblDepartment.Text = "Department";
-            // 
-            // txtStudentName
-            // 
-            this.txtStudentName.Location = new System.Drawing.Point(129, 125);
-            this.txtStudentName.Name = "txtStudentName";
-            this.txtStudentName.Size = new System.Drawing.Size(144, 20);
-            this.txtStudentName.TabIndex = 4;
-            // 
-            // txtStudentID
-            // 
-            this.txtStudentID.Location = new System.Drawing.Point(129, 157);
-            this.txtStudentID.Name = "txtStudentID";
-            this.txtStudentID.Size = new System.Drawing.Size(144, 20);
-            this.txtStudentID.TabIndex = 5;
-            // 
-            // btnLoginToCheckMarks
-            // 
-            this.btnLoginToCheckMarks.Location = new System.Drawing.Point(128, 239);
-            this.btnLoginToCheckMarks.Name = "btnLoginToCheckMarks";
-            this.btnLoginToCheckMarks.Size = new System.Drawing.Size(80, 27);
-            this.btnLoginToCheckMarks.TabIndex = 7;
-            this.btnLoginToCheckMarks.Text = "Login";
-            this.btnLoginToCheckMarks.UseVisualStyleBackColor = true;
-            // 
-            // comboDepartment
-            // 
-            this.comboDepartment.FormattingEnabled = true;
-            this.comboDepartment.Location = new System.Drawing.Point(129, 188);
-            this.comboDepartment.Name = "comboDepartment";
-            this.comboDepartment.Size = new System.Drawing.Size(144, 21);
-            this.comboDepartment.TabIndex = 8;
+            this.lblCopyright1.AutoSize = true;
+            this.lblCopyright1.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright1.Location = new System.Drawing.Point(122, 350);
+            this.lblCopyright1.Name = "lblCopyright1";
+            this.lblCopyright1.Size = new System.Drawing.Size(45, 12);
+            this.lblCopyright1.TabIndex = 32;
+            this.lblCopyright1.Text = "Copyright";
             // 
             // picBoxLogoMainForm
             // 
             this.picBoxLogoMainForm.BackColor = System.Drawing.Color.Transparent;
             this.picBoxLogoMainForm.Image = ((System.Drawing.Image)(resources.GetObject("picBoxLogoMainForm.Image")));
-            this.picBoxLogoMainForm.Location = new System.Drawing.Point(71, 12);
+            this.picBoxLogoMainForm.Location = new System.Drawing.Point(62, 21);
             this.picBoxLogoMainForm.Name = "picBoxLogoMainForm";
             this.picBoxLogoMainForm.Size = new System.Drawing.Size(189, 53);
             this.picBoxLogoMainForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picBoxLogoMainForm.TabIndex = 9;
+            this.picBoxLogoMainForm.TabIndex = 31;
             this.picBoxLogoMainForm.TabStop = false;
+            // 
+            // comboStuDepartment
+            // 
+            this.comboStuDepartment.FormattingEnabled = true;
+            this.comboStuDepartment.Location = new System.Drawing.Point(120, 235);
+            this.comboStuDepartment.Name = "comboStuDepartment";
+            this.comboStuDepartment.Size = new System.Drawing.Size(144, 21);
+            this.comboStuDepartment.TabIndex = 30;
+            // 
+            // btnLecturerLogin
+            // 
+            this.btnLecturerLogin.Location = new System.Drawing.Point(119, 286);
+            this.btnLecturerLogin.Name = "btnLecturerLogin";
+            this.btnLecturerLogin.Size = new System.Drawing.Size(80, 27);
+            this.btnLecturerLogin.TabIndex = 29;
+            this.btnLecturerLogin.Text = "Login";
+            this.btnLecturerLogin.UseVisualStyleBackColor = true;
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(120, 166);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(144, 20);
+            this.txtStudentID.TabIndex = 28;
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(120, 134);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(144, 20);
+            this.txtStudentName.TabIndex = 27;
+            // 
+            // lblStuDepartment
+            // 
+            this.lblStuDepartment.AutoSize = true;
+            this.lblStuDepartment.BackColor = System.Drawing.Color.Transparent;
+            this.lblStuDepartment.Location = new System.Drawing.Point(29, 238);
+            this.lblStuDepartment.Name = "lblStuDepartment";
+            this.lblStuDepartment.Size = new System.Drawing.Size(62, 13);
+            this.lblStuDepartment.TabIndex = 26;
+            this.lblStuDepartment.Text = "Department";
+            // 
+            // lblStudentID
+            // 
+            this.lblStudentID.AutoSize = true;
+            this.lblStudentID.BackColor = System.Drawing.Color.Transparent;
+            this.lblStudentID.Location = new System.Drawing.Point(28, 169);
+            this.lblStudentID.Name = "lblStudentID";
+            this.lblStudentID.Size = new System.Drawing.Size(58, 13);
+            this.lblStudentID.TabIndex = 25;
+            this.lblStudentID.Text = "Student ID";
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.BackColor = System.Drawing.Color.Transparent;
+            this.lblStudentName.Location = new System.Drawing.Point(29, 137);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(35, 13);
+            this.lblStudentName.TabIndex = 24;
+            this.lblStudentName.Text = "Name";
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.BackColor = System.Drawing.Color.Transparent;
+            this.lblInstructions.Location = new System.Drawing.Point(28, 103);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(122, 13);
+            this.lblInstructions.TabIndex = 23;
+            this.lblInstructions.Text = "Please enter your details";
             // 
             // studentLoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(327, 278);
+            this.ClientSize = new System.Drawing.Size(309, 370);
+            this.Controls.Add(this.txtStuPassword);
+            this.Controls.Add(this.lblStuPassword);
+            this.Controls.Add(this.lblCopyright2);
+            this.Controls.Add(this.lblCopyright1);
             this.Controls.Add(this.picBoxLogoMainForm);
-            this.Controls.Add(this.comboDepartment);
-            this.Controls.Add(this.btnLoginToCheckMarks);
+            this.Controls.Add(this.comboStuDepartment);
+            this.Controls.Add(this.btnLecturerLogin);
             this.Controls.Add(this.txtStudentID);
             this.Controls.Add(this.txtStudentName);
-            this.Controls.Add(this.lblDepartment);
+            this.Controls.Add(this.lblStuDepartment);
             this.Controls.Add(this.lblStudentID);
             this.Controls.Add(this.lblStudentName);
             this.Controls.Add(this.lblInstructions);
@@ -149,14 +196,18 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblInstructions;
-        private System.Windows.Forms.Label lblStudentName;
-        private System.Windows.Forms.Label lblStudentID;
-        private System.Windows.Forms.Label lblDepartment;
-        private System.Windows.Forms.TextBox txtStudentName;
-        private System.Windows.Forms.TextBox txtStudentID;
-        private System.Windows.Forms.Button btnLoginToCheckMarks;
-        private System.Windows.Forms.ComboBox comboDepartment;
+        private System.Windows.Forms.TextBox txtStuPassword;
+        private System.Windows.Forms.Label lblStuPassword;
+        private System.Windows.Forms.Label lblCopyright2;
+        private System.Windows.Forms.Label lblCopyright1;
         private System.Windows.Forms.PictureBox picBoxLogoMainForm;
+        private System.Windows.Forms.ComboBox comboStuDepartment;
+        private System.Windows.Forms.Button btnLecturerLogin;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Label lblStuDepartment;
+        private System.Windows.Forms.Label lblStudentID;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.Label lblInstructions;
     }
 }
