feacaadf5e118f77327644d810168c13cc2784e1
dilum1995 <dilumdesilva.edu@gmail.com>
2017-06-14 09:01:10 +0530

    lecturer main form modifications

--- UniversityRegistrationSystem/UniversityRegistrationSystem/LecturerMainForm.Designer.cs
+++ UniversityRegistrationSystem/UniversityRegistrationSystem/LecturerMainForm.Designer.cs
@@ -29,21 +29,169 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LecturerMainForm));
+            this.lblCopyright2 = new System.Windows.Forms.Label();
+            this.lblCopyright1 = new System.Windows.Forms.Label();
+            this.lblLecDepartmentView = new System.Windows.Forms.Label();
+            this.lblLecturerIDView = new System.Windows.Forms.Label();
+            this.lblLecturerNameView = new System.Windows.Forms.Label();
+            this.btnLecLogout = new System.Windows.Forms.Button();
+            this.btnLecUpdateMark = new System.Windows.Forms.Button();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.lblLecturerID = new System.Windows.Forms.Label();
+            this.lblLecturerName = new System.Windows.Forms.Label();
+            this.picBoxLogoMainForm = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogoMainForm)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblCopyright2
+            // 
+            this.lblCopyright2.AutoSize = true;
+            this.lblCopyright2.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright2.Location = new System.Drawing.Point(164, 344);
+            this.lblCopyright2.Name = "lblCopyright2";
+            this.lblCopyright2.Size = new System.Drawing.Size(27, 7);
+            this.lblCopyright2.TabIndex = 35;
+            this.lblCopyright2.Text = "C 2017";
+            // 
+            // lblCopyright1
+            // 
+            this.lblCopyright1.AutoSize = true;
+            this.lblCopyright1.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright1.Location = new System.Drawing.Point(121, 340);
+            this.lblCopyright1.Name = "lblCopyright1";
+            this.lblCopyright1.Size = new System.Drawing.Size(45, 12);
+            this.lblCopyright1.TabIndex = 34;
+            this.lblCopyright1.Text = "Copyright";
+            // 
+            // lblLecDepartmentView
+            // 
+            this.lblLecDepartmentView.AutoSize = true;
+            this.lblLecDepartmentView.BackColor = System.Drawing.Color.Transparent;
+            this.lblLecDepartmentView.Location = new System.Drawing.Point(160, 187);
+            this.lblLecDepartmentView.Name = "lblLecDepartmentView";
+            this.lblLecDepartmentView.Size = new System.Drawing.Size(86, 13);
+            this.lblLecDepartmentView.TabIndex = 33;
+            this.lblLecDepartmentView.Text = "Department here";
+            // 
+            // lblLecturerIDView
+            // 
+            this.lblLecturerIDView.AutoSize = true;
+            this.lblLecturerIDView.BackColor = System.Drawing.Color.Transparent;
+            this.lblLecturerIDView.Location = new System.Drawing.Point(160, 157);
+            this.lblLecturerIDView.Name = "lblLecturerIDView";
+            this.lblLecturerIDView.Size = new System.Drawing.Size(60, 13);
+            this.lblLecturerIDView.TabIndex = 32;
+            this.lblLecturerIDView.Text = "LecID here";
+            // 
+            // lblLecturerNameView
+            // 
+            this.lblLecturerNameView.AutoSize = true;
+            this.lblLecturerNameView.BackColor = System.Drawing.Color.Transparent;
+            this.lblLecturerNameView.Location = new System.Drawing.Point(160, 125);
+            this.lblLecturerNameView.Name = "lblLecturerNameView";
+            this.lblLecturerNameView.Size = new System.Drawing.Size(77, 13);
+            this.lblLecturerNameView.TabIndex = 31;
+            this.lblLecturerNameView.Text = "LecName here";
+            // 
+            // btnLecLogout
+            // 
+            this.btnLecLogout.Location = new System.Drawing.Point(108, 271);
+            this.btnLecLogout.Name = "btnLecLogout";
+            this.btnLecLogout.Size = new System.Drawing.Size(92, 31);
+            this.btnLecLogout.TabIndex = 30;
+            this.btnLecLogout.Text = "Logout";
+            this.btnLecLogout.UseVisualStyleBackColor = true;
+            // 
+            // btnLecUpdateMark
+            // 
+            this.btnLecUpdateMark.Location = new System.Drawing.Point(108, 231);
+            this.btnLecUpdateMark.Name = "btnLecUpdateMark";
+            this.btnLecUpdateMark.Size = new System.Drawing.Size(92, 31);
+            this.btnLecUpdateMark.TabIndex = 29;
+            this.btnLecUpdateMark.Text = "Student Details";
+            this.btnLecUpdateMark.UseVisualStyleBackColor = true;
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.BackColor = System.Drawing.Color.Transparent;
+            this.lblDepartment.Location = new System.Drawing.Point(71, 187);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(68, 13);
+            this.lblDepartment.TabIndex = 28;
+            this.lblDepartment.Text = "Department :";
+            // 
+            // lblLecturerID
+            // 
+            this.lblLecturerID.AutoSize = true;
+            this.lblLecturerID.BackColor = System.Drawing.Color.Transparent;
+            this.lblLecturerID.Location = new System.Drawing.Point(71, 157);
+            this.lblLecturerID.Name = "lblLecturerID";
+            this.lblLecturerID.Size = new System.Drawing.Size(66, 13);
+            this.lblLecturerID.TabIndex = 27;
+            this.lblLecturerID.Text = "Lecturer ID :";
+            // 
+            // lblLecturerName
+            // 
+            this.lblLecturerName.AutoSize = true;
+            this.lblLecturerName.BackColor = System.Drawing.Color.Transparent;
+            this.lblLecturerName.Location = new System.Drawing.Point(71, 125);
+            this.lblLecturerName.Name = "lblLecturerName";
+            this.lblLecturerName.Size = new System.Drawing.Size(83, 13);
+            this.lblLecturerName.TabIndex = 26;
+            this.lblLecturerName.Text = "Lecturer Name :";
+            // 
+            // picBoxLogoMainForm
+            // 
+            this.picBoxLogoMainForm.BackColor = System.Drawing.Color.Transparent;
+            this.picBoxLogoMainForm.Image = ((System.Drawing.Image)(resources.GetObject("picBoxLogoMainForm.Image")));
+            this.picBoxLogoMainForm.Location = new System.Drawing.Point(64, 20);
+            this.picBoxLogoMainForm.Name = "picBoxLogoMainForm";
+            this.picBoxLogoMainForm.Size = new System.Drawing.Size(189, 53);
+            this.picBoxLogoMainForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxLogoMainForm.TabIndex = 25;
+            this.picBoxLogoMainForm.TabStop = false;
             // 
             // LecturerMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(327, 362);
+            this.Controls.Add(this.lblCopyright2);
+            this.Controls.Add(this.lblCopyright1);
+            this.Controls.Add(this.lblLecDepartmentView);
+            this.Controls.Add(this.lblLecturerIDView);
+            this.Controls.Add(this.lblLecturerNameView);
+            this.Controls.Add(this.btnLecLogout);
+            this.Controls.Add(this.btnLecUpdateMark);
+            this.Controls.Add(this.lblDepartment);
+            this.Controls.Add(this.lblLecturerID);
+            this.Controls.Add(this.lblLecturerName);
+            this.Controls.Add(this.picBoxLogoMainForm);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "LecturerMainForm";
             this.Text = "LecturerMainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogoMainForm)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCopyright2;
+        private System.Windows.Forms.Label lblCopyright1;
+        private System.Windows.Forms.Label lblLecDepartmentView;
+        private System.Windows.Forms.Label lblLecturerIDView;
+        private System.Windows.Forms.Label lblLecturerNameView;
+        private System.Windows.Forms.Button btnLecLogout;
+        private System.Windows.Forms.Button btnLecUpdateMark;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.Label lblLecturerID;
+        private System.Windows.Forms.Label lblLecturerName;
+        private System.Windows.Forms.PictureBox picBoxLogoMainForm;
     }
 }
