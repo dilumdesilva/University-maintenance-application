b7d9545868b4da4b6ddff4634e8d729d6e672e5f
dilum1995 <dilumdesilva.edu@gmail.com>
2017-06-14 08:50:09 +0530

    students marks view form edited

--- UniversityRegistrationSystem/UniversityRegistrationSystem/studentMarksForm.Designer.cs
+++ UniversityRegistrationSystem/UniversityRegistrationSystem/studentMarksForm.Designer.cs
@@ -33,9 +33,11 @@
             this.comboDepartment = new System.Windows.Forms.ComboBox();
             this.dgvStudentsMarks = new System.Windows.Forms.DataGridView();
             this.btnEmailGenerator = new System.Windows.Forms.Button();
-            this.btnClose = new System.Windows.Forms.Button();
+            this.btnStuBackToLogin = new System.Windows.Forms.Button();
             this.lblMessage = new System.Windows.Forms.Label();
             this.picBoxLogoMainForm = new System.Windows.Forms.PictureBox();
+            this.lblCopyright2 = new System.Windows.Forms.Label();
+            this.lblCopyright1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvStudentsMarks)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxLogoMainForm)).BeginInit();
             this.SuspendLayout();
@@ -44,7 +46,7 @@
             // 
             this.lblNameReplace.AutoSize = true;
             this.lblNameReplace.BackColor = System.Drawing.Color.Transparent;
-            this.lblNameReplace.Location = new System.Drawing.Point(21, 115);
+            this.lblNameReplace.Location = new System.Drawing.Point(36, 115);
             this.lblNameReplace.Name = "lblNameReplace";
             this.lblNameReplace.Size = new System.Drawing.Size(35, 13);
             this.lblNameReplace.TabIndex = 0;
@@ -53,7 +55,7 @@
             // comboDepartment
             // 
             this.comboDepartment.FormattingEnabled = true;
-            this.comboDepartment.Location = new System.Drawing.Point(231, 112);
+            this.comboDepartment.Location = new System.Drawing.Point(246, 112);
             this.comboDepartment.Name = "comboDepartment";
             this.comboDepartment.Size = new System.Drawing.Size(158, 21);
             this.comboDepartment.TabIndex = 1;
@@ -61,34 +63,34 @@
             // dgvStudentsMarks
             // 
             this.dgvStudentsMarks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvStudentsMarks.Location = new System.Drawing.Point(24, 147);
+            this.dgvStudentsMarks.Location = new System.Drawing.Point(39, 147);
             this.dgvStudentsMarks.Name = "dgvStudentsMarks";
             this.dgvStudentsMarks.Size = new System.Drawing.Size(365, 156);
             this.dgvStudentsMarks.TabIndex = 2;
             // 
             // btnEmailGenerator
             // 
-            this.btnEmailGenerator.Location = new System.Drawing.Point(24, 318);
+            this.btnEmailGenerator.Location = new System.Drawing.Point(39, 318);
             this.btnEmailGenerator.Name = "btnEmailGenerator";
             this.btnEmailGenerator.Size = new System.Drawing.Size(95, 29);
             this.btnEmailGenerator.TabIndex = 3;
             this.btnEmailGenerator.Text = "Email a report";
             this.btnEmailGenerator.UseVisualStyleBackColor = true;
             // 
-            // btnClose
+            // btnStuBackToLogin
             // 
-            this.btnClose.Location = new System.Drawing.Point(136, 318);
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(97, 29);
-            this.btnClose.TabIndex = 4;
-            this.btnClose.Text = "Close";
-            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnStuBackToLogin.Location = new System.Drawing.Point(307, 318);
+            this.btnStuBackToLogin.Name = "btnStuBackToLogin";
+            this.btnStuBackToLogin.Size = new System.Drawing.Size(97, 29);
+            this.btnStuBackToLogin.TabIndex = 4;
+            this.btnStuBackToLogin.Text = "Home";
+            this.btnStuBackToLogin.UseVisualStyleBackColor = true;
             // 
             // lblMessage
             // 
             this.lblMessage.AutoSize = true;
             this.lblMessage.BackColor = System.Drawing.Color.Transparent;
-            this.lblMessage.Location = new System.Drawing.Point(51, 115);
+            this.lblMessage.Location = new System.Drawing.Point(66, 115);
             this.lblMessage.Name = "lblMessage";
             this.lblMessage.Size = new System.Drawing.Size(151, 13);
             this.lblMessage.TabIndex = 5;
@@ -98,22 +100,46 @@
             // 
             this.picBoxLogoMainForm.BackColor = System.Drawing.Color.Transparent;
             this.picBoxLogoMainForm.Image = ((System.Drawing.Image)(resources.GetObject("picBoxLogoMainForm.Image")));
-            this.picBoxLogoMainForm.Location = new System.Drawing.Point(109, 27);
+            this.picBoxLogoMainForm.Location = new System.Drawing.Point(124, 27);
             this.picBoxLogoMainForm.Name = "picBoxLogoMainForm";
             this.picBoxLogoMainForm.Size = new System.Drawing.Size(189, 53);
             this.picBoxLogoMainForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picBoxLogoMainForm.TabIndex = 11;
             this.picBoxLogoMainForm.TabStop = false;
             // 
+            // lblCopyright2
+            // 
+            this.lblCopyright2.AutoSize = true;
+            this.lblCopyright2.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright2.Location = new System.Drawing.Point(231, 389);
+            this.lblCopyright2.Name = "lblCopyright2";
+            this.lblCopyright2.Size = new System.Drawing.Size(27, 7);
+            this.lblCopyright2.TabIndex = 24;
+            this.lblCopyright2.Text = "C 2017";
+            // 
+            // lblCopyright1
+            // 
+            this.lblCopyright1.AutoSize = true;
+            this.lblCopyright1.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright1.Location = new System.Drawing.Point(188, 385);
+            this.lblCopyright1.Name = "lblCopyright1";
+            this.lblCopyright1.Size = new System.Drawing.Size(45, 12);
+            this.lblCopyright1.TabIndex = 23;
+            this.lblCopyright1.Text = "Copyright";
+            // 
             // studentMarksForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(413, 359);
+            this.ClientSize = new System.Drawing.Size(445, 406);
+            this.Controls.Add(this.lblCopyright2);
+            this.Controls.Add(this.lblCopyright1);
             this.Controls.Add(this.picBoxLogoMainForm);
             this.Controls.Add(this.lblMessage);
-            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStuBackToLogin);
             this.Controls.Add(this.btnEmailGenerator);
             this.Controls.Add(this.dgvStudentsMarks);
             this.Controls.Add(this.comboDepartment);
@@ -134,8 +160,10 @@
         private System.Windows.Forms.ComboBox comboDepartment;
         private System.Windows.Forms.DataGridView dgvStudentsMarks;
         private System.Windows.Forms.Button btnEmailGenerator;
-        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnStuBackToLogin;
         private System.Windows.Forms.Label lblMessage;
         private System.Windows.Forms.PictureBox picBoxLogoMainForm;
+        private System.Windows.Forms.Label lblCopyright2;
+        private System.Windows.Forms.Label lblCopyright1;
     }
 }
